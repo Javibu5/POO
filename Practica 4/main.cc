6796685fbf33da45bd45492a286ef8175963f0c4
Javibu5 <adfjb77@gmail.com>
2017-11-13 13:52:33 +0100

    anyade main.cc

---
+++ Practica 4/main.cc
@@ -0,0 +1,22 @@
+//programa principal
+
+int main()
+{
+
+	cout<<"Introduce la opción que quieras elegir :"<<endl;
+	cout<<"1. Carga los juegadores del fichero jugadores.txt"<<endl;
+	cout<<"2. Guarda los jugadores en jugadores.txt"<<endl;
+	cout<<"3. Ver el estado de la ruleta"<<endl;
+	cout<<"4. Hacer girar la ruleta mostrando el numero que ha salido ,
+	 los premios de cada jugador y lo que gana/pierde la banca" <<endl;
+	 cout<<"5. Elimina un jugador de la mesa"<<endl;
+	 cout<<"6. Añadir jugador a la mesa"<<endl;
+	 cout<<"7. Salir"<<endl;
+
+
+	int opcion;
+	switch (opcion){
+
+	}
+	return 0;
+}
